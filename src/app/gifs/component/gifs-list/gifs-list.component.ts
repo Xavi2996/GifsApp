5e0805f5aab207377f5d8502e65cafbed102c8f7
xavier <xavier.cobos.29@hotmail.com>
2026-03-23 12:40:49 -0500

    feat: componetización galería gifs trending

---
+++ src/app/gifs/component/gifs-list/gifs-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { GifsListItemComponent } from './gifs-list-item/gifs-list-item.component';
+
+@Component({
+  selector: 'gifs-list',
+  imports: [GifsListItemComponent],
+  templateUrl: './gifs-list.component.html',
+  styleUrl: './gifs-list.component.css',
+})
+export class GifsListComponent {
+  gifs = input.required<string[]>();
+}
